fa99832313d1a1c18485358b9fb10094810ff17c
catapultsoftworks <brich@Brandons-MacBook-Pro-2.local>
2013-12-17 10:02:28 -0500

    added openresty stuff

--- init.pp
+++ init.pp
@@ -20,6 +20,15 @@ $tns_admin_path = "${oracle_install_path}/11.2/client64/network/admin"
 $ld_library_path = "${oracle_install_path}/11.2/client64/lib"
 $oracle_home_path ="${oracle_install_path}/11.2/client64"
 
+# openresty environment variables
+$openresty_package_url = "https://s3.amazonaws.com/OpenRestyPackage/ngx_openresty-1.2.8.6.tar.tar.tar.gz"
+
+$base_dir = "/usr/local"
+$openresty_home = "${base_dir}/openresty"
+$openresty_src = "${openresty_home}/src"
+$openresty_filename = "ngx_openresty-1.2.8.6"
+$targz_suffix = ".tar.gz"
+
 # DO THIS!
 #include download_oracle
 #include oracle_instant
@@ -31,6 +40,7 @@ include download_oracle
 include oracle_instant
 include rvm
 include install_things_with_rvm
+include openresty
 
 # RVM module must exist and have had the libcurl dependency file modified to work
 # on Amazon Linux (by changing the default to libcurl-devel in /manifests/dependencies/centos.pp)
@@ -188,3 +198,84 @@ class oracle_instant {
   }
 }
 
+
+
+class openresty {
+  package {'readline-devel':
+	ensure =>	latest,
+	provider =>	yum
+  }
+  ->
+  package {'pcre-devel':
+	ensure =>	latest,
+	provider =>	yum
+  }
+  ->
+  package {'openssl-devel':
+	ensure =>	latest,
+	provider =>	yum
+  }
+  ->
+  package {'perl':
+	ensure =>	installed,
+	provider =>	yum
+  }
+  ->
+  package {'make':
+	ensure =>	installed,
+	provider =>	yum
+  }
+  ->
+  file {"openresty install dir":
+	ensure =>	directory,
+  	path => 	"${base_dir}/openresty",
+	mode =>		"0755",
+	owner =>	$user
+  }
+  ->	
+  file {"openresty install dir src":
+	ensure =>	directory,
+  	path => 	"${openresty_src}",
+	mode =>		"0755",
+	owner =>	$user
+  }
+  ->	
+  exec { 'download package':
+        command => "wget ${openresty_package_url} -O ${openresty_src}/${openresty_filename}${targz_suffix}",
+        creates => "${openresty_src}/${openresty_filename}${targz_suffix}"
+  }
+  ->
+  file {"${openresty_filename}${targz_suffix}":
+	ensure =>	file,
+	path => 	"${openresty_src}/${openresty_filename}${targz_suffix}",
+	source => 	"${openresty_src}/${openresty_filename}${targz_suffix}",
+	mode => 	"0755",
+	owner => 	$user
+  }
+  ->
+  exec {"tar -xzf ${openresty_filename}${targz_suffix}":
+	user => 	$user,
+	cwd =>		$openresty_src
+  }
+  ->
+  exec {"${openresty_src}/${openresty_filename}/configure --with-luajit":
+	cwd =>		"${openresty_src}/${openresty_filename}",
+	user =>		$user
+  }
+  ->
+  exec {"make -j2":
+	cwd =>		"${openresty_src}/${openresty_filename}",
+	user =>		$user
+  }
+  ->
+  exec {"make install":
+	cwd =>		"${openresty_src}/${openresty_filename}"
+  }
+  ->
+  # ENVIRONMENTAL SETTINGS
+  exec {"bash -c 'echo \"export PATH=\\\$PATH:${openresty_home}/bin\" >> /home/${user}/.bashrc'":
+	user => 	$user
+  }
+	
+  
+}
