68b7e6866e36ca36c5fd8564a72dfcc30fab5f7b
asejdina <asejdina@gmail.com>
2015-03-10 09:30:21 -0400

    altered bulk_load.sh file

--- bulk_load.sh
+++ bulk_load.sh
@@ -6,7 +6,7 @@ read -r -p "Would you like to bulk load your database? [y/n] " response
 echo $response
 
 if [ $response = y ]; then
-  curl -H "Content-Type: application/json charset=utf-8" -d @terms.json http://localhost:3000/bulk?admin=true > log.text 
+  curl -H "Content-Type: application/json" --data @terms.json http://localhost:3000/bulk?admin=true > log.text 
   echo "Your data has been loaded and your environment is ready."
 else
   echo "Your environment is ready."
